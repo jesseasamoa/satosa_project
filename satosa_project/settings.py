200332174b3d5c053ca58c5b11964b09051fe71e
Jesse Asamoa-Krodua <89225154+jesseasamoa@users.noreply.github.com>
2022-09-19 14:15:47 +0000

    Update settings.py

--- satosa_project/settings.py
+++ satosa_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%f_o_321o+ge06tuir(y1497#w9=8n5j3u$p9_8qj$z#u45n3%'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
